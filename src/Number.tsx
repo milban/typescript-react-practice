79b525d2cb6366752a6e6e6377f6bc5990411234
JunHyeock Park <wnsgur6311@gmail.com>
2019-09-26 16:11:09 +0900

    Ts with styled-components

--- src/Number.tsx
+++ src/Number.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import styled from "styled-components"
 
-const Container = styled.span``
+const Container = styled.span<{ isBlue: boolean }>`
+  color: ${props => (props.isBlue ? "blue" : "red")};
+`
 
 interface IProps {
   count: number
 }
 
-const Number: React.FC<IProps> = ({ count }) => <Container>{count}</Container>
+const Number: React.FC<IProps> = ({ count }) => <Container isBlue={count < 10}>{count}</Container>
 
 export default Number
